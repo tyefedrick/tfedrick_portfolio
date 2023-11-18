ec70527f4c6d3b7b021cd2a3b08d7ec0bae38bf3
Tyler Fedrick <tyefedrick@gmail.com>
2023-11-17 21:05:58 -0500

    Fix the blog link.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,7 +4,8 @@ class BlogPostsController < ApplicationController
   end
 
   def show  
-    @log_post = BlogPost.find(params{:id})
+    binding.pry
+    @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to blog_posts_path
   end
