0252ca61d4439e125f6ab9d5b635e5b31e4515fa
Tyler Fedrick <tyefedrick@gmail.com>
2023-11-18 14:42:10 -0500

    Working Flash Notices

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -19,7 +19,20 @@ class BlogPostsController < ApplicationController
     @blog_post = BlogPost.new(title: params[:blog_post][:title], body: params[:blog_post][:body])
     @blog_post.save
 #I need to add a notice. 
-    redirect_to blog_post_url(@blog_post),notice: "Blog Post Created Successfully!"
+   if @blog_post.save
+    flash[:notice] = "Blog Post Created Successfully!"
+    redirect_to blog_post_url(@blog_post)
+   else 
+    render :new
+   end
+#redirect_to blog_post_url(@blog_post),notice: "Blog Post Created Successfully!"
   end
+  
+private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
+
 end
 
