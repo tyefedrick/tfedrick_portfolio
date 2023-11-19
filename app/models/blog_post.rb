014970cf3de3f0c0ab56cd800eec1416590edf4b
Tyler Fedrick <tyefedrick@gmail.com>
2023-11-19 14:33:55 -0500

    Comments added to remind me of generate model

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,2 +1,3 @@
+#rails generate model BlogPost title:string body:text  
 class BlogPost < ApplicationRecord
 end
--- app/models/book_review.rb
+++ app/models/book_review.rb
@@ -1,2 +1,3 @@
+#rails generate model BookReview title:string rating:integer body:text notes:text
 class BookReview < ApplicationRecord
 end
