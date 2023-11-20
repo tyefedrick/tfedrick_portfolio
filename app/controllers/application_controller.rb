7497960c47b27a6e0adcbc5af835999f7b17a5a7
Tyler Fedrick <tyefedrick@gmail.com>
2023-11-19 21:01:28 -0500

    Notes about my scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+#rails generate scaffold User username:string first_name:string last_name:string email:string
+#rake db:migrate is to complete the migrate
+# rake db:rollback is to revert the changes 
 class ApplicationController < ActionController::Base
 end
