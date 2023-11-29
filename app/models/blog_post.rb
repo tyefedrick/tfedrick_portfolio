678381cd7fe7f2d0fdab57dd0823559bf7e9993e
Tyler Fedrick <tyefedrick@gmail.com>
2023-11-28 23:09:55 -0500

    Trix editor has been added to the project

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,3 +1,10 @@
 #rails generate model BlogPost title:string body:text  
 class BlogPost < ApplicationRecord
+    before_save :set_content
+
+    private
+  
+    def set_content
+      self.content = ActionController::Base.helpers.sanitize(trix_content)
+    end 
 end
