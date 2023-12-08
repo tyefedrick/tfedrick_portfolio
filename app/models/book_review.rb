308a31df6b39836366d5f44fa55dea0de00b28a3
Tyler Fedrick <tyefedrick@gmail.com>
2023-12-07 19:45:41 -0500

    Added  formatting and rich text to bookreview

--- app/models/book_review.rb
+++ app/models/book_review.rb
@@ -1,3 +1,6 @@
 #rails generate model BookReview title:string rating:integer body:text notes:text
 class BookReview < ApplicationRecord
+    has_rich_text:body
+
+    validates :body, presence: true
 end
