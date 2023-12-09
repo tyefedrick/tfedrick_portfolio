cd7705315942eb3b620139adbad91d07ed127ee1
Tyler Fedrick <tyefedrick@gmail.com>
2023-12-09 15:48:06 -0500

    Light/Dark mode toggle works, One option will DLT

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,32 @@
+//= require jquery3
+//= require popper
+//= require bootstrap
+
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap"
 import "./controllers"
-
-
-
-
-
 import "trix"
 import "@rails/actiontext"
+
+
+document.addEventListener('turbolinks:load', function() {
+    console.log("Page loaded with TurboLinks");
+    
+    const btnSwitch = document.getElementById('btnSwitch');
+    if (btnSwitch) {
+      console.log("Button element found");
+      
+      btnSwitch.addEventListener('click', () => {
+        console.log("Button clicked");
+        
+        const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+        if (currentTheme === 'dark') {
+          document.documentElement.setAttribute('data-bs-theme', 'light');
+        } else {
+          document.documentElement.setAttribute('data-bs-theme', 'dark');
+        }
+      });
+    }
+  });
+
