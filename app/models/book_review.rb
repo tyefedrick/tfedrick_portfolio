aebd32714db0ef0decd075afe340270c4d5bc292
Tyler Fedrick <tyefedrick@gmail.com>
2023-12-19 21:41:00 -0500

    Minor fix to create book review for authors

--- app/models/book_review.rb
+++ app/models/book_review.rb
@@ -1,6 +1,7 @@
 #rails generate model BookReview title:string rating:integer body:text notes:text
 class BookReview < ApplicationRecord
     has_rich_text:body
+    has_and_belongs_to_many :authors
     has_and_belongs_to_many :genres
     validates :body, presence: true
 end
